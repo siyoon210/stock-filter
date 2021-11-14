02c96b93475d7a7e7a64d9813c51d8d17e3c1129
siyoon210 <siyoon210@gmail.com>
2021-11-14 22:03:34 +0900

    amplify allssu 3편, amplify 사용하기

--- aws-amplify/amplify-next-allssu/pages/_app.js
+++ aws-amplify/amplify-next-allssu/pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 import {ChakraProvider} from '@chakra-ui/react'
+import {Amplify} from 'aws-amplify'
+import awsConfigure from '../src/aws-exports'
+
+Amplify.configure(awsConfigure)
 
 function MyApp({Component, pageProps}) {
     return (
--- aws-amplify/amplify-next-allssu/pages/index.js
+++ aws-amplify/amplify-next-allssu/pages/index.js
@@ -1,7 +1,50 @@
 import {Center, Box, Text, Input, Button, List, ListItem, ListIcon} from '@chakra-ui/react'
 import {CheckCircleIcon} from "@chakra-ui/icons";
+import {useEffect, useState} from "react";
+import {API, graphqlOperation} from 'aws-amplify'
+import {listTodos} from "../src/graphql/queries";
+import {createTodo} from "../src/graphql/mutations";
 
 export default function Home() {
+    const [todos, setTodos] = useState([]);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        try {
+            const result = await API.graphql(graphqlOperation(listTodos));
+            setTodos(result.data.listTodos.items);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const handleChange = (e) => {
+        e.currentTarget.name === "subject" ? setName(e.currentTarget.value) : null;
+        e.currentTarget.name === "description" ? setDescription(e.currentTarget.value) : null;
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const result = await API.graphql(graphqlOperation(createTodo, {
+                input: {
+                    name,
+                    description
+                }
+            }));
+            setTodos([...todos, result.data.createTodo]);
+            setName("");
+            setDescription("");
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
         <Center>
             <Box>
@@ -12,32 +55,27 @@ export default function Home() {
                 </Center>
                 <form>
                     <Box mt={2}>
-                        <Input name={"subject"} placeholder={"제목"} onChange={null} value={null}/>
-                        <Input name={"description"} placeholder={"내용"} onChange={null} value={null}/>
+                        <Input name={"subject"} placeholder={"제목"} onChange={handleChange} value={name}/>
+                        <Input name={"description"} placeholder={"내용"} onChange={handleChange} value={description}/>
                     </Box>
                     <Center>
                         <Box mt={2}>
-                            <Button colorScheme={"teal"} type={"submit"}>
+                            <Button colorScheme={"teal"} type={"submit"} onClick={handleSubmit}>
                                 등록
                             </Button>
                         </Box>
                     </Center>
                 </form>
                 <List spacing={3}>
-                    <ListItem>
-                        <Box fontWeight={"semibold"}>
-                            <ListIcon as={CheckCircleIcon} color={"teal.500"}/>
-                            할일 제목이 들어간다.
-                        </Box>
-                        <Box color={"gray.500"}>할일 내용이 들어간다. </Box>
-                    </ListItem>
-                    <ListItem>
-                        <Box fontWeight={"semibold"}>
-                            <ListIcon as={CheckCircleIcon} color={"teal.500"}/>
-                            할일 제목이 들어간다.
-                        </Box>
-                        <Box color={"gray.500"}>할일 내용이 들어간다. </Box>
-                    </ListItem>
+                    {todos.map((todo, index) => {
+                        return (<ListItem key={index}>
+                            <Box fontWeight={"semibold"}>
+                                <ListIcon as={CheckCircleIcon} color={"teal.500"}/>
+                                {todo.name}
+                            </Box>
+                            <Box color={"gray.500"}>{todo.description} </Box>
+                        </ListItem>)
+                    })}
                 </List>
             </Box>
         </Center>
