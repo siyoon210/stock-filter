c098399014f194fa0827f6a806e66d2c3663d86e
siyoon <siyoon@woowahan.com>
2021-12-05 06:37:26 +0900

    initial commit

---
+++ src/main/java/me/siyoon/stockfilter/StockFilterApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.stockfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockFilterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/siyoon/stockfilter/StockFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.siyoon.stockfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
